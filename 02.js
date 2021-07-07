639353ab5f54103e638d1666f63cfc9b041e3f1f
Chayanin <arm.crazyplay@gmail.com>
2021-07-07 09:43:34 +0700

    first commit

---
+++ 02.js
@@ -0,0 +1,14 @@
+function allInOrNot(input){
+  /* Your code here */
+}
+
+//Please don't change following lines
+const input1 = [1, 2, 3]
+const input2 = ["hello", true, 0]
+const input3 = ["Only", "string", "array"]
+const input4 = []
+
+console.log( allInOrNot( input1))
+console.log( allInOrNot( input2))
+console.log( allInOrNot( input3))
+console.log( allInOrNot( input4))
---
+++ 04.js
@@ -0,0 +1,21 @@
+function cloneMonster(monster, amount){
+  /* Your code here */
+}
+
+//Please don't change following lines
+const monster1 = {
+  type: 'Zergling', legs: 4, speed: 'very fast'
+}
+const amount1 = 2
+
+const monster2 = {
+  type: 'Zombie', dmg: 2, hp: 10
+}
+const amount2 = 3
+
+const monster3 = {}
+const amount3 = 4
+
+console.log( cloneMonster(monster1, amount1))
+console.log( cloneMonster(monster2, amount2))
+console.log( cloneMonster(monster3, amount3))
